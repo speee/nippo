a0935bb9034256dd161bca22a6e1eb94295f1661
Hirokazu Nishioka <hiro@nisshiee.org>
2016-05-15 15:45:23 +0900

    Tune db column size

--- db/migrate/20160514032533_devise_create_users.rb
+++ db/migrate/20160514032533_devise_create_users.rb
@@ -1,12 +1,12 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :email,    null: false
-      t.string :provider, null: false
-      t.string :uid,      null: false
-      t.string :name,     null: false
-      t.string :image,    null: false
-      t.string :token,    null: false
+      t.string :email,    null: false, limit: 128
+      t.string :provider, null: false, limit: 32
+      t.string :uid,      null: false, limit: 32
+      t.string :name,     null: false, limit: 64
+      t.string :image,    null: false, limit: 128
+      t.string :token,    null: false, limit: 128
 
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
@@ -18,6 +18,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
   end
 end
