ef6d8b2bea3dbca42f891607de2c6b90c940289f
Hirokazu Nishioka <hiro@nisshiee.org>
2016-05-15 13:41:08 +0900

    Introduce devise & omniauth-google-oauth2

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :trackable, :omniauthable
+
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+
+    unless user
+      user = User.create(name:     auth.info.name,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         token:    auth.credentials.token)
+    end
+    user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+  }
 end
