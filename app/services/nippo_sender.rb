a8a13573f7038f19009d159006177b2a4c71a7f7
Hirokazu Nishioka <hiro@nisshiee.org>
2016-05-25 19:31:20 +0900

    Add `Content-Type` header

--- app/services/nippo_sender.rb
+++ app/services/nippo_sender.rb
@@ -11,21 +11,23 @@ class NippoSender
   Gmail = Google::Apis::GmailV1
   ME = 'me'
   SUBJECT_ENCODING = '-jWM'
-  CONTENT_TYPE = 'message/rfc822'
+  TEXT_PLANE = 'text/plain; charset=UTF-8'
+  RFC822 = 'message/rfc822'
 
   def send
     gmail = Gmail::GmailService.new
     gmail.authorization = user.token
 
     message = RMail::Message.new
-    message.header.to      = Settings.nippo.send_to
-    message.header.from    = user.email
-    message.header.subject = NKF.nkf(SUBJECT_ENCODING, subject)
+    message.header.to              = Settings.nippo.send_to
+    message.header.from            = user.email
+    message.header.subject         = NKF.nkf(SUBJECT_ENCODING, subject)
+    message.header['Content-Type'] = TEXT_PLANE
     message.body = nippo.body
 
     gmail.send_user_message(ME,
       upload_source: StringIO.new(message.to_s),
-      content_type: CONTENT_TYPE)
+      content_type: RFC822)
 
     nippo.update!(sent_at: Time.zone.now)
   end
