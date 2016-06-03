29c2aaea9325e5d6a2f5fd97550561ca6fc78769
Hirokazu Nishioka <hiro@nisshiee.org>
2016-06-03 17:52:33 +0900

    Implement logic for default nippo date

--- spec/models/nippo_spec.rb
+++ spec/models/nippo_spec.rb
@@ -3,4 +3,38 @@ RSpec.describe Nippo do
   subject { FG.build(:nippo) }
 
   it { is_expected.to be_valid }
+
+  describe '.default_report_date' do
+    shared_examples '.default_report_date' do |input, output|
+      let(:now) { Time.zone.parse(input) }
+      let(:expected) { Date.parse(output) }
+
+      it 'returns expected date' do
+        expect(Nippo.default_report_date(now)).to eq expected
+      end
+    end
+
+    context 'Mon-Fri after 10AM' do
+      include_examples '.default_report_date', '2016-06-06 10:00', '2016-06-06'
+      include_examples '.default_report_date', '2016-06-07 10:00', '2016-06-07'
+      include_examples '.default_report_date', '2016-06-08 10:00', '2016-06-08'
+      include_examples '.default_report_date', '2016-06-09 10:00', '2016-06-09'
+      include_examples '.default_report_date', '2016-06-10 10:00', '2016-06-10'
+    end
+
+    context 'Tue-Fri before 10AM' do
+      include_examples '.default_report_date', '2016-06-07 09:59', '2016-06-06'
+      include_examples '.default_report_date', '2016-06-08 09:59', '2016-06-07'
+      include_examples '.default_report_date', '2016-06-09 09:59', '2016-06-08'
+      include_examples '.default_report_date', '2016-06-10 09:59', '2016-06-09'
+    end
+
+    context 'before Mon 10AM' do
+      include_examples '.default_report_date', '2016-06-11 09:59', '2016-06-10'
+      include_examples '.default_report_date', '2016-06-11 10:00', '2016-06-10'
+      include_examples '.default_report_date', '2016-06-12 09:59', '2016-06-10'
+      include_examples '.default_report_date', '2016-06-12 10:00', '2016-06-10'
+      include_examples '.default_report_date', '2016-06-13 09:59', '2016-06-10'
+    end
+  end
 end
