ffbeacb2a6ecc94b4c5da90fc7ba33c732bfc45f
Hirokazu Nishioka <hiro@nisshiee.org>
2016-06-15 20:47:30 +0900

    Turn on secure flag for session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_nippo_session'
+Rails.application.config.session_store(
+  :cookie_store,
+  key: '_nippo_session',
+  secure: Rails.env.production?,
+)
