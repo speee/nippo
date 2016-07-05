a172f038fa6a5322a02651f9b6340e790d10ec57
Hirokazu Nishioka <hiro@nisshiee.org>
2016-07-05 14:50:02 +0900

    Add test of NippoSender

--- spec/services/nippo_sender_spec.rb
+++ spec/services/nippo_sender_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 RSpec.describe NippoSender do
-  subject { NippoSender.new(nippo: FG.create(:nippo, status: :draft)) }
+  let(:nippo) { FG.create(:nippo, status: :draft) }
+  subject { NippoSender.new(nippo: nippo) }
 
   it { is_expected.to be_valid }
 
@@ -12,7 +13,7 @@ RSpec.describe NippoSender do
   it { is_expected.to be_invalid_on(:nippo).with(FG.create(:nippo, status: :sent)) }
 
   context 'when validation fails' do
-    before { subject.nippo.update(status: :sending) }
+    before { nippo.update(status: :sending) }
 
     it 'returns nil and stores error' do
       expect(subject.run).to be_nil
@@ -20,7 +21,7 @@ RSpec.describe NippoSender do
     end
 
     it 'stores error and does NOT change nippo status' do
-      expect { subject.run }.not_to change(subject.nippo, :status)
+      expect { subject.run }.not_to change(nippo, :status)
     end
   end
 
@@ -28,7 +29,7 @@ RSpec.describe NippoSender do
     it 'returns nil, stores error, and update nippo status to :draft' do
       expect(subject.run).to be_nil
       expect(subject.errors).not_to be_empty
-      expect(subject.nippo.draft?).to eq true
+      expect(nippo.draft?).to eq true
     end
   end
 
@@ -36,7 +37,27 @@ RSpec.describe NippoSender do
     it 'returns true and update nippo status to :sent' do
       expect(subject.run).to eq true
       expect(subject.errors).to be_empty
-      expect(subject.nippo.sent?).to eq true
+      expect(nippo.sent?).to eq true
+    end
+  end
+
+  describe '#from' do
+    let(:user) { nippo.user }
+    let(:template) { user.template }
+
+    context 'when from_name is empty' do
+      before { template.update(from_name: '') }
+      it 'returns the value `email`' do
+        expect(subject.send(:from)).to eq user.email
+      end
+    end
+
+    context 'when from_name contains japanese' do
+      before { template.update(from_name: '日本語') }
+      it 'returns MIME encoded' do
+        @actual = subject.send(:from)
+        expect(NKF.nkf('-mw', @actual)).to eq "日本語 <#{user.email}>"
+      end
     end
   end
 end
