e4cb5a3bc91ae8cd13e7ef4c316d0a7eeb6912d7
iida <hayato.iida@speee.jp>
2016-07-16 14:34:01 +0900

    fix rubocop. ```rubocop -a```

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
 Doorkeeper.configure do
   # Change the ORM that doorkeeper will use (needs plugins)
   orm :active_record
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    current_user || warden.authenticate!(:scope => :user)
+    current_user || warden.authenticate!(scope: :user)
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare
