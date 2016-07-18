69f13f49144c1e442319b9760b875829407224f7
Hirokazu Nishioka <hiro@nisshiee.org>
2016-07-18 13:32:52 +0900

    Introduce Sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_application_raven_context if Rails.env.production?
+  before_action :enable_js_raven               if Rails.env.production?
+
+  private
+
+  def set_application_raven_context
+    Raven.extra_context(params: params.to_h, url: request.url)
+  end
+
+  def enable_js_raven
+    gon.raven = {
+      dsn: Settings.sentry.public_dsn,
+    }
+  end
 end
