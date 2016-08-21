afb3547acf32b745e1ccdc569f76ed0be04ad520
iida <hayato.iida.0213@gmail.com>
2016-08-22 03:02:26 +0900

    add api/nippos

---
+++ app/controllers/api/nippos_controller.rb
@@ -0,0 +1,12 @@
+class Api::NipposController < PrivateController
+  include NippoHandleable
+
+  def index
+    @recent_nippos = Nippo
+                         .where.not(sent_at: nil)
+                         .order(sent_at: :desc)
+                         .limit(100)
+    render json: @recent_nippos
+  end
+
+end
