ddc39004100a91dc60aaf59c4418b1f6d22e8587
iida <hayato.iida.0213@gmail.com>
2016-08-24 23:33:42 +0900

    add nippo api spec

---
+++ spec/controllers/api/nippos_controller_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+RSpec.describe Api::NipposController do
+  describe 'GET index' do
+    let(:token) { double :acceptable? => true }
+    let(:user) { FG.create(:user) }
+    before do
+      controller.stub(:doorkeeper_token) { token }
+      controller.stub(:current_user) { user }
+    end
+    it 'returns success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
