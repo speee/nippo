4c3d54fb9cdd0ff5724dd69ac0e1716e7728e7d3
iida <hayato.iida.0213@gmail.com>
2016-08-25 20:57:35 +0900

    add "api/nippos" test

--- spec/controllers/api/nippos_controller_spec.rb
+++ spec/controllers/api/nippos_controller_spec.rb
@@ -13,10 +13,17 @@ RSpec.describe Api::NipposController do
     before do
       controller.stub(:doorkeeper_token) { token }
       controller.stub(:current_user) { user }
+      @nippo = FG.create(:nippo, sent_at: Time.zone.now)
     end
+
     it 'returns success' do
       get :index
       expect(response).to have_http_status(:success)
+      @json = JSON.parse(response.body)
+      @json.map do |nippo|
+        expect(nippo['subject']).to eq @nippo.subject
+        expect(nippo['body']).to eq @nippo.body
+      end
     end
   end
 end
